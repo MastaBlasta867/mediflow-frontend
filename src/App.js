34284b25bfac40f63b2ea10504ba3945a91d744b
MastaBlasta867 <pomolefe1986@gmail.com>
2026-05-21 02:49:28 +0000

    Final update for today: slide-in drawer + layout polish

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { Box, Drawer, DrawerContent, useDisclosure } from "@chakra-ui/react";
+import { Box, Drawer, DrawerContent, Text, useDisclosure } from "@chakra-ui/react";
 import NavBar from "./components/Layout/NavBar";
 
 function App() {
@@ -8,13 +8,33 @@ function App() {
     <Box>
       <NavBar onOpen={onOpen} />
 
-      <Drawer isOpen={isOpen} placement="left" onClose={onClose}>
-        <DrawerContent p={6}>
-          <Box>Menu goes here</Box>
+      {/* Mobile Drawer Menu */}
+      <Drawer
+        isOpen={isOpen}
+        placement="left"
+        onClose={onClose}
+        motionPreset="slideInLeft"
+      >
+        <DrawerContent
+          p={6}
+          bg="white"
+          boxShadow="xl"
+          borderRight="1px solid rgba(0,0,0,0.1)"
+        >
+          <Box fontSize="lg" fontWeight="bold" mb={6}>
+            Menu
+          </Box>
+
+          <Box display="flex" flexDirection="column" gap={4}>
+            <Text cursor="pointer">Dashboard</Text>
+            <Text cursor="pointer">Appointments</Text>
+            <Text cursor="pointer">Patients</Text>
+          </Box>
         </DrawerContent>
       </Drawer>
 
-      <Box p={10}>
+      {/* Page Content */}
+      <Box pt="100px" p={10}>
         <h1>Welcome to MediFlow</h1>
       </Box>
     </Box>
